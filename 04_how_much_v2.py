93a35073438848eae74fc061cbb143db76b90628
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2023-03-02 10:35:19 +1300

    Add files via upload

--- 04_how_much_v2.py
+++ 04_how_much_v2.py
@@ -23,9 +23,8 @@ def num_check(question, low, high):
 
 while True:
     how_much = num_check("How much would you"
-                        " like to play with? ", 0, 10)
+                         " like to play with? ", 0, 10)
 
     print("You will be spending ${}".format(how_much))
-
 
 # Print the
